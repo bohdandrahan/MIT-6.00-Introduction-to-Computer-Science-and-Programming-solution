6301ba022a9a5be60ab9dc975a277db435eedd37
bohdandrahan <31550739+bohdandrahan@users.noreply.github.com>
2017-09-06 22:57:21 -0400

    Delete ps0.py

--- ps0.py
+++
@@ -1,9 +0,0 @@
-# Problem Set 0
-# Name: Bohdan Drahan 
-# Collaborators: 
-# Time:0:20 
-#
-
-lname = raw_input('Enter your last name:')
-fname = raw_input('Enter your first name:')
-print fname, lname
